228430bda0276fd8e3f6ed9e4285efca3e1e5cdd
silverwings <silverwingsw@icloud.com>
2017-06-09 05:53:09 +0800

    Modified BookmarksServer.py PORT

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -104,7 +104,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
                 "Couldn't fetch URI '{}'. Sorry!".format(longuri).encode())
 
 if __name__ == '__main__':
-    post = int(os.environ.get('PORT', 8080))
+    post = int(os.environ.get('PORT', 8000))
     server_address = ('', 8000)
     httpd = ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
