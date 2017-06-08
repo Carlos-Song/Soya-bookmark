e09b0ce092b157d543d7e2e48cf9a5a5b7471941
silverwings <silverwingsw@icloud.com>
2017-06-09 05:17:35 +0800

    Modified BookmarkServer.py

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -6,7 +6,7 @@ import http.server
 import requests
 import os
 import threading
-from SocketServer import ThreadingMixIn
+from socketserver import ThreadingMixIn
 from urllib.parse import unquote, parse_qs
 
 memory = {}
